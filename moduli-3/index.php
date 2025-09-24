78a148627034db9886275aaef05c22b02a6f789c
Aron-Ramadani <aronramadani2010@gmail.com>
2025-09-24 20:45:15 +0200

    switch&loops

--- moduli-3/index.php
+++ moduli-3/index.php
@@ -1,46 +1,52 @@
 <?php
-    $num = -5;
-    if ($num < 0){
-        echo ("$num is less then 0");
-    }
 
-    echo("<br>");
+$day= 'Monday';
+switch($day){
+    case'Monday':
+        echo "Its Monday";
+    break;
+    case'Tuesday':
+        echo "Its Tuesday";
+    break;
+    case'Wednesday':
+        echo "Its Wednesday";
+    break;
+    case'Thursday':
+        echo "Its Thursday";
+    break;
+    case'Friday':
+        echo "Its Friday";
+    break;
+    case'Saturday':
+        echo "Its Saturday";
+    break;
+    case'Sunday':
+        echo "Its Sunday";
+    break;
+    default:
+    echo"Its an invalid day";
+    break;
 
-    $age = 15;
-    if (($age > 12) && ($age < 20)) {
-        echo("You are a teenager");
-    }
+}
 
-    echo("<br>");
 
-    if($age < 20) {
-        echo"you are under 18";
-    }
-    else{
-        echo"you are an adult";
-    }
+echo "<br>";
 
-    echo("<br>");
+$x=1;
+while ($x <=5){
+    echo"The number is $x<br>";
+    $x++;
+}
 
-    if($num < 0) {
-        echo"The value of $num is negative";
-    }
-    else if($num == 0) {
-        echo"The value of $num is 0";
-    }
-    else {
-        echo"The value of $num is positive";
-    }
+echo "<br>";
 
-    echo("<br>");
+$y = 1;
+do{
+    echo"the number is $y<br>";
+    $y++;
+}while($y<=5);
 
-    $a=50;
-    $b=10;
 
-    if($a==$b){
-        echo("True");
-    }   else{
-        echo("False");
-    }
+
 
 ?>
