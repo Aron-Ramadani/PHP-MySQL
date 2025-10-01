16ece6bf935fbec0ea2e5a8a4830b76cbd3baa2b
unknown <Student@DIGITALSCHOOL.LOCAL>
2025-10-01 20:28:22 +0200

    scope veriables and arrays

---
+++ moduli-3/arrays.php
@@ -0,0 +1,37 @@
+<?php
+
+    // $sports =['Football','Basketball','Volleyball'];
+    // echo $sports[0];
+
+    // "<br>"
+    
+    // echo end($sports);
+
+    // "<br>"
+
+    // echo count($sports);
+
+    // "<br>";
+
+    // $sports = array('Football','Basketball','Volleyball');
+    // for($i = 0; $i < 3; $i++){
+    //     echo $sports[$i],"\n";
+    // }
+
+    // $sports = array('Football','Basketball','Volleyball');
+    // array_push($sports,"Golf");
+    // var_dump($sports);
+
+    // $sports = array('Football','Basketball','Volleyball');
+    // array_pop($sports);
+    // var_dump($sports);
+
+    // $sports = array('Football','Basketball','Volleyball');
+    // array_unshift($sports,'Tennis');
+    // var_dump($sports);
+
+     $sports = array('Football','Basketball','Volleyball');
+    array_shift($sports,);
+    var_dump($sports);
+
+?>
